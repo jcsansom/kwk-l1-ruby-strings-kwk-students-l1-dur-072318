a25242d5f28a0853749a31c4552e4b08801ca12d
jcsansom <jayceesansom@gmail.com>
2018-07-24 13:10:49 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -8,3 +8,11 @@ date=
 time=
 host_name=
 
+puts "Dear " + guest_name +",
+ 
+You are cordially invited to the " + party_name + "on " + date "at " + time ". Please RSVP no later than October 30.
+ 
+Sincerely,"
+
++host_name
+ 
